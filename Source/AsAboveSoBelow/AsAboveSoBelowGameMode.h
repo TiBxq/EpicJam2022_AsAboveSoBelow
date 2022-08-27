85365e012b465bae24b89b5cdfe116c469ed6ee7
Bogdan <b.savenko@dragonslake.com>
2022-08-27 17:56:59 +0200

    setup project

---
+++ Source/AsAboveSoBelow/AsAboveSoBelowGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsAboveSoBelowGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAsAboveSoBelowGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAsAboveSoBelowGameMode();
+};
+
+
+
